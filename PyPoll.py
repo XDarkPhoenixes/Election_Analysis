d4c1612338be20e4ef723c318131393ad71fdfdc
Tron zhou <tronziheng.zhou@mail.utoronto.ca>
2021-07-08 14:27:29 -0400

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -24,48 +24,58 @@ winning_percentage = 0
 with open(file_to_load) as election_data:
     # To do: read and analyze the data here.
     file_reader = csv.reader(election_data)
-
     # Print the header row.
     headers = next(file_reader)
-    
     # Print each row in the CSV file.
     for row in file_reader:
         # 2. Add to the total vote count.
         total_votes += 1
         # Print the candidate name from each row
         candidate_name = row[2]
-        
         # If the candidate does not match any existing candidate then add the name
         if candidate_name not in candidate_options:
             #add the candidate name to the candidate list
             candidate_options.append(candidate_name)
             #begin tracking that candidate's vote count.
             candidate_votes[candidate_name] = 0
-
         # Add a vote to that candidate's count.
         candidate_votes[candidate_name] += 1    
-    #iterate through the candidate list. 
-    for candidate_name in candidate_votes:
-        # Retrieve vote count of a candidate.
-        votes = candidate_votes[candidate_name]
-        # Calculate the percentage of votes.
-        vote_percentage = float(votes)/float(total_votes) * 100
-        # to do: Print out each candidate's name, vote count, and percentage of vote to
-        # the terminal
-        print(f'{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n')
-        
-        # Determine the winning vote cout and candidate
-        # Determine if the votes are greater than the winning count. 
-        if (votes > winning_count) and (vote_percentage > winning_percentage):
-            #If true then set winning_count = votes and winning_percent = vote_percentage.
-            winning_count = votes
-            winning_percentage = vote_percentage
-            winning_candidate = candidate_name
 
-# To do: print out 
-winning_candidate_summary = (f"----------------------\n"
-f"Winner: {winning_candidate}\n"
-f"Winning Vote Count: {winning_count:,}\n"
-f"Winning Percentage: {winning_percentage:.1f}%\n"
-f"---------------------------")
-print(winning_candidate_summary)
+# Save the results to our text file.
+with open(file_to_save, "w") as txt_file:
+    # Print the final vote count to the terminal.
+    election_results = (f"Election Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
+
+    for candidate_name in candidate_votes:
+        # Retrieve vote count and percentage.
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        # Print each candidate, their voter count, and percentage to the
+        # terminal.
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        # Print each candidate, their voter count, and percentage to the terminal.
+        print(candidate_results)
+        # Save the candidate results to our text file.
+        txt_file.write(candidate_results)
+        # Determine winning vote count, winning percentage, and candidate.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_candidate = candidate_name
+            winning_percentage = vote_percentage
+    # Print the winning candidates' results to the terminal.
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary) 
+    # Save the winning candidate's name to the text file.
+    txt_file.write(winning_candidate_summary)
+    
